4b239b93a71fefa9d0f811a4444aba0592f5a8e3
benni maxon <benni.maxon@gmail.com>
2023-01-25 17:22:00 -0800

    cleaned up setFrom

--- src/components/ExecutePassButton.js
+++ src/components/ExecutePassButton.js
@@ -2,11 +2,11 @@ import React, { useContext } from 'react';
 import { GameContext } from '../context/GameContext.js';
 import Card from './Card';
 
-export default function ExecutePassButton({ setFrom }) {
+export default function ExecutePassButton() {
   const { selectedCard, to, from, passCard } = useContext(GameContext);
   return (
     <div className="execute-button" onClick={() => passCard(selectedCard)}>
-      Pass <Card card={selectedCard} setFrom={setFrom} /> from {from} to {to}
+      Pass <Card card={selectedCard} /> from {from} to {to}
     </div>
   );
 }
